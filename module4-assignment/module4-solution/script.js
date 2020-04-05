10d8bf680a21680ff9ac00aca5299eca1172eac6
pravakar paltasingh <61041599+pravakar1001@users.noreply.github.com>
2020-04-05 19:52:51 +0530

    Update script.js

--- module4-assignment/module4-solution/script.js
+++ module4-assignment/module4-solution/script.js
@@ -1,5 +1,5 @@
 (function(){
-    var names = ["alok","bishal","rajveer", "pravakar","suman","Nitya","jeevan","jyoti","juhi","umala","jasmin","lalit"];
+    var names = ["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
     for(var i=0;i<names.length;i++){
         var firstLetter = names[i].charAt(0).toLowerCase();
         if(firstLetter === 'j'){
