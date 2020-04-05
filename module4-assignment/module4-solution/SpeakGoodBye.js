6f2b2687811e0649dd7102bafd3952e8ae21a1b4
pravakar paltasingh <61041599+pravakar1001@users.noreply.github.com>
2020-04-05 19:58:45 +0530

    Delete SpeakGoodBye.js

--- module4-assignment/module4-solution/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-    var bySpeaker = {};
-    var speakWord = "Good Bye";
-    bySpeaker.speak = function (name){
-        console.log(speakWord+" "+name);
-    }
-
-    window.bySpeaker = bySpeaker;
-})(window);
